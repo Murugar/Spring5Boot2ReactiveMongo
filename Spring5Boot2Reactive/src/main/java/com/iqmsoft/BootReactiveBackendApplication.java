85f3c410d680066cb920ef72c536133d09b6c851
Davanon <davanon2014@gmail.com>
2018-01-15 11:50:20 +0530

    First

---
+++ Spring5Boot2Reactive/src/main/java/com/iqmsoft/BootReactiveBackendApplication.java
@@ -0,0 +1,12 @@
+package com.iqmsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReactiveBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootReactiveBackendApplication.class, args);
+  }
+}
